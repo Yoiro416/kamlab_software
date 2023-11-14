76472d83a9effe9448ee37f78986fec83ea4852c
Yoiro416 <75517744+Yoiro416@users.noreply.github.com>
2023-11-14 22:14:47 +0900

    upload codes:
    show images

---
+++ python/single/time_UTC.py
@@ -0,0 +1,13 @@
+import time
+import datetime
+
+while True:
+    dt_now_utc= datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=0)))
+    print(dt_now_utc.strftime('%Y年%m月%d日  %a  ')+
+          '\n'+dt_now_utc.strftime('%H:%M:%S')+
+          "\033[1F",end="") #年月日、曜日、時間の表示
+    time.sleep(1)  #更新する時間の設定
+    
+    # '\033[1F'はANSIエスケープコード
+    # \033[nF : カーソルを上にn行移動した後、その行の先頭に移動
+    # https://qiita.com/kuroitu/items/f18acf87269f4267e8c1
